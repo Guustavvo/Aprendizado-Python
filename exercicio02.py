dcd3e16eeba742d25527c7b7cd79ac8938dd7829
Guustavvo <101718484+Guustavvo@users.noreply.github.com>
2022-10-19 12:04:32 -0300

    cadeia de string

---
+++ exercicio02.py
@@ -0,0 +1,10 @@
+
+# uma string é imutavel, mesmo utilizando uma replace (transformação de string) quando printar na tela a varial n ira mudar, so ira mudar se eu salvar criando uma outra varialvel com "replace"
+
+
+#frase = ("curso em video Python")
+#frase = frase.replace("Python" , "android")
+#print(frase)
+
+nome = input("digite um nome")
+print("silva" in nome)
