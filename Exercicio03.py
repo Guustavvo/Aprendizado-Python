a5929fa8d0dd902c846ad1c7c9e25caf3f3872ad
Guustavvo <101718484+Guustavvo@users.noreply.github.com>
2022-10-25 10:10:31 -0300

    Estrutura condicional

---
+++ Exercicio03.py
@@ -0,0 +1,29 @@
+# Estrura condicional simples é quando se trabalha apenas com o (IF) e estrutura condicional composta é quando utilizamos (IF e Else)
+
+''''''''''''
+#Estrutura condicional composta
+
+#tempo = int(input("Quantos anos tem seu carro"))
+#if tempo <=3:
+    #print("carro novo")
+
+#else:
+    #print("carro velho")
+
+''''''''''''
+#Estrutura condicional simples.
+#nome = str(input("digite seu nome"))
+#if nome =="Gustavo":
+    #print("seja bem vindo!") 
+#print("bom dia, {}!".format(nome))
+
+#num = int(input("digite um numero"))
+#parouimpar = num% 2
+#if parouimpar == 0:
+    #print("é par")
+#else:
+    #print("é impar")
+
+
+
+
