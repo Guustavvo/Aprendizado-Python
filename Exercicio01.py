cceac351d8fb7ec260ad4710cbfc6efcb40ce154
Guustavvo <101718484+Guustavvo@users.noreply.github.com>
2022-10-25 09:17:26 -0300

    "comentario"

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,8 +1,8 @@
 ## treino
 
 
-medida = float(input("uma distancia em metros"))
-cm = medida * 100
-mm = medida * 1000
-print("a medida de {}m corresponde a {}cm e a tantos {}mm".format(medida, cm, mm))
+#medida = float(input("uma distancia em metros"))
+#cm = medida * 100
+#mm = medida * 1000
+#print("a medida de {}m corresponde a {}cm e a tantos {}mm".format(medida, cm, mm))
 
