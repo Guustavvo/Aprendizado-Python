7ed1aaf0f666fe29c7c8cc766ec6776653eba4f1
Guustavvo <101718484+Guustavvo@users.noreply.github.com>
2022-11-08 15:52:35 -0300

    comentario

---
+++ automatização.py
@@ -0,0 +1,7 @@
+#1 - Entrar no site https://admin.google.com/
+#2 - clicar em Add a user
+#3- colocar as informaçoes do user
+#4- clicar em adicionar usuario
+#5- e gerar um pdf com as credencias dele.
+
+
---
+++ exercicio04.py
@@ -0,0 +1,10 @@
+nome = str(input("digite seu nome"))
+if nome == "gustavo":
+    print("seja bem vindo")
+
+else:
+    print("operação invalida")
+
+
+
+
